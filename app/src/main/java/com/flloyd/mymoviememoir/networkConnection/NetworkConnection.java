2f4a0b68917db5f0324b59af96b58867a53a98ea
Flloyd <dsouzaflloyd.11>
2020-04-29 21:11:11 +1000

    Login Done

---
+++ app/src/main/java/com/flloyd/mymoviememoir/networkConnection/NetworkConnection.java
@@ -0,0 +1,39 @@
+package com.flloyd.mymoviememoir.networkConnection;
+
+import android.util.Log;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkConnection {
+    private OkHttpClient client = null;
+    private String results;
+    public static final MediaType JSON =
+            MediaType.parse("application/json; charset=utf-8");
+    public NetworkConnection(){
+        client=new OkHttpClient();
+    }
+    private static final String BASE_URL =
+            "http://10.0.2.2:8080/Assignment1/webresources/";
+
+    //"http://10.0.2.2:8080/Assignment1/webresources/restws.person/";
+
+
+    public String getCredintials(String email,String password){
+        final String methodPath = "restws.credentials/Authenticate/" + email + "/" + password;
+        Log.i("Flloyd","request" + methodPath.toString());
+        Request.Builder builder = new Request.Builder();
+        builder.url(BASE_URL + methodPath);
+
+        Request request = builder.build();
+        try {
+            Response response = client.newCall(request).execute();
+            results = response.body().string();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return results;
+    }
+}
