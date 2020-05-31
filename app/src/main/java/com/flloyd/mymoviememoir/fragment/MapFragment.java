9061fd8d9a393f5cc04541c7dcc3c81255a424b9
Flloyd <dsouzaflloyd.11>
2020-05-31 14:02:15 +1000

    Task 10 Complete 100

--- app/src/main/java/com/flloyd/mymoviememoir/fragment/MapFragment.java
+++ app/src/main/java/com/flloyd/mymoviememoir/fragment/MapFragment.java
@@ -6,31 +6,32 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.flloyd.mymoviememoir.R;
-import com.flloyd.mymoviememoir.networkConnection.GeoCodingAPI;
+import com.flloyd.mymoviememoir.networkConnection.NetworkConnection;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
@@ -38,6 +39,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private SupportMapFragment mapFragment;
     private GoogleMap mMap;
     private LatLng currentlocation;
+    private LatLng cinemaLoc;
+    private NetworkConnection networkConnection = null;
+    private Map<String,String> cinemaAdress;
+    private Map<String,LatLng> cinema;
     public MapFragment() {
 
     }
@@ -48,6 +53,18 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+        networkConnection = new NetworkConnection();
+        cinema = new HashMap<>();
+        cinemaAdress = new HashMap<>();
+
+        cinemaAdress.put("The Astor Theatre","The Astor Theatre 3182");
+        cinemaAdress.put("Village Cinemas Jam Factory","Village Cinemas Jam Factory 3141");
+        cinemaAdress.put("Now Showing PTY Ltd.","Now Showing PTY Ltd. 3181");
+        cinemaAdress.put("Barefoot Cinema","Barefoot Cinema Rippon Lea Estate 3185");
+        cinemaAdress.put("Hotys","1341 Dandenong Rd, Chadstone VIC 3148");
+        cinemaAdress.put("Classic Cinemas","9 Gordon St, Elsternwick VIC 3185");
+
+
 
         String streetAddress = "";
         String stateCode = "";
@@ -63,7 +80,18 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             postCode = j.getString("postcode");
         } catch (Exception ignored) { }
 
+        //getCinema getCinema = new getCinema();
+        //getCinema.execute();
+
         String address = streetAddress + " " + stateCode + " " + postCode;
+        Log.i("Flloyd","address" + address);
+
+
+        for (Map.Entry cinemaAddressEntry : cinemaAdress.entrySet()) {
+
+            cinemaLoc =   getLocationFromAddress(this.getContext(), (String) cinemaAddressEntry.getValue());
+            cinema.put((String) cinemaAddressEntry.getKey(),cinemaLoc);
+        }
 
         currentlocation = getLocationFromAddress(this.getContext(),address);
 
@@ -83,8 +111,24 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.addMarker(new MarkerOptions().position(currentlocation).title("My location"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentlocation));
+        mMap.addMarker(new MarkerOptions().position(currentlocation).title("My location")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        for (Map.Entry me : cinema.entrySet()) {
+            mMap.addMarker(new MarkerOptions().position((LatLng) me.getValue()).title((String) me.getKey()));
+        }
+
+
+
+        mMap.addMarker(new MarkerOptions().position(cinemaLoc).title("cinema"));
+
+        float zoomLevel = (float) 15.0;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation,
+                zoomLevel));
+
+    }
+
+
  /*Zoom levels
  1: World
  5: Landmass/continent
@@ -92,12 +136,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
  15: Streets
  20: Buildings
  */
-        float zoomLevel = (float) 15.0;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation,
-                zoomLevel));
-    }
-
-
 
 
     public LatLng getLocationFromAddress(Context context,String strAddress) {
@@ -106,23 +144,52 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         List<Address> address;
         LatLng p1 = null;
 
-        try {
-            address = coder.getFromLocationName(strAddress, 5);
-            if (address == null) {
-                return null;
+        if(coder.isPresent()) {
+            try {
+                address = coder.getFromLocationName(strAddress, 1);
+                if (address == null) {
+                    return null;
+                }
+                Address location = address.get(0);
+                location.getLatitude();
+                location.getLongitude();
+
+                Log.i("Flloyd", "address" + location.getLatitude() + location.getLongitude());
+
+                p1 = new LatLng(location.getLatitude(), location.getLongitude());
+
+            } catch (Exception ex) {
+
+                ex.printStackTrace();
             }
-            Address location = address.get(0);
-            location.getLatitude();
-            location.getLongitude();
-
-            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
-
-        } catch (Exception ex) {
-
-            ex.printStackTrace();
         }
-
         return p1;
     }
+
+
+    private class getCinema extends AsyncTask<String,Void,JSONArray> {
+        @Override
+        protected JSONArray doInBackground(@NotNull String... params) {
+            return networkConnection.getCinema();
+        }
+        @Override
+        protected void onPostExecute(JSONArray result) {
+            if (result != null){
+                Log.i("Flloyd ","JSON ARRAY: "+ result.toString());
+
+                try {
+                    for (int i = 0; i < result.length(); i++){
+                        String cinemaName = result.getJSONObject(i).getString("cinemaname");
+                        String cinemaPostCode = result.getJSONObject(i).getString("cinemapostcode");
+                        LatLng cinemaLoc = getLocationFromAddress(getContext(),cinemaName + " " + cinemaPostCode);
+                        cinema.put(cinemaName,cinemaLoc);
+                        Log.i("Flloyd ","Cinema Lat Long "+ cinemaLoc.toString());
+                    }
+                }catch (Exception ignored){
+                }
+            }
+        }
+    }
+
 
 }
